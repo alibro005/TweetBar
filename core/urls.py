69e79ae382844fc8429b390c4c78b3b80b58c25d
alibro005 <itsalibro9@gmail.com>
2026-02-07 21:07:16 +0500

    ops: Add a landing page of tweetbar

--- core/urls.py
+++ core/urls.py
@@ -20,13 +20,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
     path("tweet/", include("tweet.urls")),
-
     path("accounts/", include("django.contrib.auth.urls")),
-
-
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
