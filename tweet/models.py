53cfc58fe52f6fb3a3798f05c8d9d583c04c10e6
alibro005 <itsalibro9@gmail.com>
2026-02-07 20:50:39 +0500

    feat: Add a user base functionality

--- tweet/models.py
+++ tweet/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+
 # Create your models here.
 
 User = get_user_model()
 
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=500)
-    photo = models.ImageField(upload_to='tweet_photos/', blank=True, null=True)
+    photo = models.ImageField(upload_to="tweet_photos/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.text[:50]}"
-    
